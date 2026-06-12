23558c60b347f53f918b4a7660b0edbb9840e262
Christina Kalk <kalk@med.uni-frankfurt.de>
2026-06-12 11:00:49 +0200

    log CPM for Scatterplots not percentage and offset 0.5 and 1 as in voom

--- Riboseq_analysis_pipeline/alignments/correlation_plots_transcriptomic.py
+++ Riboseq_analysis_pipeline/alignments/correlation_plots_transcriptomic.py
@@ -23,14 +23,14 @@ def read_idx_stats(idx_stat):
     # skip the last row that is just a star and two dots
     idx_stat_df = idx_stat_df.iloc[:-1, :]
     # percentage and add a pseudocount
-    idx_stat_df[f'{idx_name}_mapping_perc'] = idx_stat_df['mapped'] / \
-        sum(idx_stat_df['mapped'])
-    idx_stat_df[f'{idx_name}_log_mapping_perc'] = idx_stat_df[f'{idx_name}_mapping_perc'].apply(
-        lambda x: math.log(x+0.000001))
-    idx_stat_df[f'{idx_name}_log_mapping_reads'] = idx_stat_df['mapped'].apply(
-        lambda x: math.log(x+0.000001))
+    idx_stat_df[f'{idx_name}_mapping_perc'] = (idx_stat_df['mapped'] + 0.5) / \
+        (sum(idx_stat_df['mapped']) + 1)
+    idx_stat_df[f'{idx_name}_cpm'] = idx_stat_df[f'{idx_name}_mapping_perc'] * 1e6
+    idx_stat_df[f'{idx_name}_log_cpm'] = idx_stat_df[f'{idx_name}_mapping_perc'].apply(
+        lambda x: math.log(x))
+
     idx_stat_df = idx_stat_df[[
-        'tID', f'{idx_name}_mapping_perc', f'{idx_name}_log_mapping_reads', f'{idx_name}_log_mapping_perc']]
+        'tID', f'{idx_name}_mapping_perc', f'{idx_name}_cpm', f'{idx_name}_log_cpm']]
     return idx_stat_df, idx_name
 
 
@@ -47,14 +47,20 @@ def main():
 
     # calculate pearson correlation
     r, _ = sp.stats.spearmanr(
-        a=idx_merged_df[f'{idx_name1}_log_mapping_perc'], b=idx_merged_df[f'{idx_name2}_log_mapping_perc'])
-    scat_plot = sbn.scatterplot(data=idx_merged_df,
-                                x=f'{idx_name1}_log_mapping_perc', y=f'{idx_name2}_log_mapping_perc')
+        a=idx_merged_df[f'{idx_name1}_log_cpm'], b=idx_merged_df[f'{idx_name2}_log_cpm'])
+    # Pearson correlation
+    pearson_r, _ = sp.stats.pearsonr(
+        x=idx_merged_df[f'{idx_name1}_log_cpm'], y=idx_merged_df[f'{idx_name2}_log_cpm'])
 
-    scat_plot.set(xlabel=f"{idx_name1} log percentage", ylabel=f"{idx_name2} log percentage",
-                  title='Scatterplot of log scaled mapping percentage of genes')
+    scat_plot = sbn.scatterplot(data=idx_merged_df,
+                                x=f'{idx_name1}_log_cpm', y=f'{idx_name2}_log_cpm')
+
+    scat_plot.set(xlabel=f"{idx_name1} log cpm", ylabel=f"{idx_name2} log cpm",
+                  title='Scatterplot of log cpm of genes')
     second_rep = '_'.join(idx_name2.split('_')[-3:])
     ax = plt.gca()  # Get a matplotlib's axes instance
+    plt.text(.05, .85, "Pearson's r  = {:.2f}".format(
+        pearson_r), transform=ax.transAxes)
     plt.text(.05, .8, "Spearman's r ={:.2f}".format(r), transform=ax.transAxes)
     plt.savefig(os.path.join(
         out_dir, f'scatterplot_spearman_{idx_name1}_{second_rep}.png'), bbox_inches='tight', dpi=300)
